7e3fe27123f9184eb09197a9df0fb0f8db74d7ef
Jonathan Silva <jonathansilvads@hotmail.com>
2023-11-22 10:52:58 -0400

    test db

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,7 +7,8 @@ const router = Router();
 router.get('/', async (req, res) => {
     
     // const result = await pool.query('SELECT users.name, level.name AS level_name, level.level AS user_level FROM users JOIN level ON users.level = level.level')
-    const result = await pool.query('SELECT NOW()')
+    // const result = await pool.query('SELECT NOW()')
+    const result = await pool.query('SELECT * FROM users')
     console.log(result)
     res.json(result.rows[0].now);
 
